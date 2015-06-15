e169762d269fb83e057a912d906434f2e7813631
释然 <shiran565@gmail.com>
2015-06-15 19:31:25 +0800

    init
    init

---
+++ assets/js/sea-config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/6/8.
+ */
+// Set configuration
+seajs.config({
+    base: "./assets/js/sea-modules/",
+    alias: {
+        "$": "zepto/1.1.3/dist/zepto"
+    }
+});
