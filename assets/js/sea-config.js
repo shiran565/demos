8a6f09dfd24e6f8567e6208e8b6ccde6ade44695
shiran565 <shiran565@gmail.com>
2015-06-17 18:26:53 +0800

    导航部分结构

--- assets/js/sea-config.js
+++ assets/js/sea-config.js
@@ -3,7 +3,7 @@
  */
 // Set configuration
 seajs.config({
-    base: "./assets/js/sea-modules/",
+    base: "./assets/js/spm_modules/",
     alias: {
         "$": "zepto/1.1.3/dist/zepto"
     }
--- assets/js/spm_modules/zepto/1.1.3/index.js
+++ assets/js/spm_modules/zepto/1.1.3/index.js
@@ -2,6 +2,5 @@ require('./src/zepto');
 require('./src/event');
 require('./src/ajax');
 require('./src/form');
-require('./src/touch');
 require('./src/ie');
 module.exports = window.Zepto;
