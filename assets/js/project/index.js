4c4d9f07137245f77701ea9baee0c4115c0df7b2
shiran565 <gewei565@gmail.com>
2015-06-23 20:45:27 +0800

    首页样式调整

--- assets/js/project/index.js
+++ assets/js/project/index.js
@@ -1,13 +1,21 @@
-/**
- * Created by Administrator on 2015/6/8.
- */
-define(function(require,exports,module){
-    var $ = require("$");
-    $(function () {
-        $("#J_submitForm").on({"touchstart":function(){
-            $(this).addClass("pass-button-full-disabled");
-        },"touchend touchcancel":function(){
-            $(this).removeClass("pass-button-full-disabled");
-        }});
-    })
+var bannerSwipe = new Swipe($('#J_slider')[0], {
+    startSlide: 0,
+    speed: 400,
+    auto: 3000,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: true,
+    callback: function(index, elem) {},
+    transitionEnd: function(index, elem) {}
+});
+
+var contentSwipe = new Swipe($('#J_main-content')[0], {
+    startSlide: 0,
+    speed: 400,
+    auto: false,
+    continuous: false,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: function(index, elem) {},
+    transitionEnd: function(index, elem) {}
 });
