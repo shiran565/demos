543dfb324f9a22fb9c1acc6d0e26fe0b48729e72
shiran565 <gewei565@gmail.com>
2015-07-03 18:13:26 +0800

    imgs gallery

--- assets/js/project/detail.js
+++ assets/js/project/detail.js
@@ -1,3 +1,22 @@
 /**
  * Created by Administrator on 2015/7/3.
  */
+$(function(){
+
+    (function(){
+        $("#J_gallery img").eq(0).on("load", function () {
+            var width = $("#J_gallery").width();
+            var height = $(this).parent().height()+20;
+            $("#J_gallery").width(width)
+                .height(height)
+                .css("overflow","hidden")
+                .perfectScrollbar();
+
+        });
+    }())
+
+
+
+
+
+});
