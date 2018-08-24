de79ac9c41fd040a89017aee3f16d8d2cd941d69
gewei <gewei@bbktel.com>
2018-08-24 21:34:43 +0800

    clean

--- game-center/finish.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    Coupon.overdueOutput();
-});
--- mall/js/project/membercenter/express/index.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    'use strict';
-    $('a.yuantong').on('tap',function(){
-        $('form.yuantong').get(0).submit();
-    });
-    $('a.ems').on('tap',function(){
-        $('form.ems').get(0).submit();
-    });
-}());
